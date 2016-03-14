f80c82d9a0adc281e3cbeabd03a1f7294aa90c54
jref <aleksandrnvk@ukr.net>
2016-03-14 17:09:37 +0200

    Update Main.java

--- src/com/brainacad/oop/teststring2/Main.java
+++ src/com/brainacad/oop/teststring2/Main.java
@@ -20,9 +20,9 @@ public class Main {
         }
         /*StringBuilder objects cannot call toCharArray, therefore, to return char[] we need to convert
         * StringBuilder to String, and then String to Char[] by calling toCharArray*/
-        String resultString = sb.substring(0);
-        char[] result = resultString.toCharArray();
-        return result;
+        String resultString = sb.substring(0);  //if you want to convert StringBuilder to String use toString() method or constructor of String
+        char[] result = resultString.toCharArray(); //you can concat this 2 lines of code
+        return result;                              //like return resultString.toCharArray();
         /*The easier solution could be if we set the return to void and simply used
         * sout(arrayElement) in if(){}, yet the Lab asks us "return an array of distinct (unique) chars (char[] ),"
         * so I had no choice but to make it ugly*/
